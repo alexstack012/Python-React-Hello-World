95e85cdd2283f3f8ab581c950110fb2cdf4f90db
Alex Stack <alex_stack012@live.com>
2021-10-20 11:29:11 -0400

    Add files via upload

---
+++ Python-React - HelloWorld/frontend/client/src/App.js
@@ -0,0 +1,13 @@
+import { TodoPage } from './Pages/TodoPage';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoPage />
+    </div>
+  );
+}
+
+export default App;
